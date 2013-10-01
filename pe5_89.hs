d39a47bc26087179eb646863eb5fc1dba0261651
Regan Sarwas @ Bob <rsarwas@gmail.com>
2013-10-01 13:02:00 -0800

    Solved 89

--- pe5_89.hs
+++ pe5_89.hs
@@ -4,7 +4,9 @@
 -- A text file contains one thousand numbers written in valid, but not necessarily minimal, Roman numerals;
 -- that is, they are arranged in descending units and obey the subtractive pair rule
 -- (see http://projecteuler.net/about=roman_numerals for the definitive rules for this problem).
--- Answer: 
+-- Answer: 743  (time real 0m0.015s)
+-- Reached level 3 (75 correct problems) only 11888 members (3.46%) have reached this level.  => 343584 members
+
 
 -- Converts a String containing a Roman numeral to an Int
 numeralToInt :: String -> Int 
@@ -29,13 +31,32 @@ numeralToInt rn = sum $ foldl mergeLesser [] (map digits rn)
 
 -- Converts an Int into a Roman numeral (String) in minimal form
 intToNumeral :: Int -> String          
-intToNumeral n = show n            
+intToNumeral n =
+  let (m,r) = n `divMod` 1000
+      (c,r') = r `divMod` 100            
+      (x,i) = r' `divMod` 10
+      hundreds x
+        | x <  4 = (replicate x 'C')      
+        | x == 4 = "CD"      
+        | x <  9 = 'D':(replicate (x-5) 'C')      
+        | x == 9 = "CM"      
+      tens x   
+        | x <  4 = (replicate x 'X')      
+        | x == 4 = "XL"      
+        | x <  9 = 'L':(replicate (x-5) 'X')      
+        | x == 9 = "XC"      
+      ones x  
+        | x <  4 = (replicate x 'I')      
+        | x == 4 = "IV"      
+        | x <  9 = 'V':(replicate (x-5) 'I')      
+        | x == 9 = "IX"      
+  in (replicate m 'M') ++ (hundreds c) ++ (tens x) ++ (ones i)
 
 
 -- Converts a valid Roman numeral into minimal form
 minimal :: String -> String
 minimal n = let v = numeralToInt n
-            in show v
+            in intToNumeral v
 
 
 main = do
