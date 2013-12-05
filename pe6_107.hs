05095a99c1491e71fbb1c9d2ea77cc875fc5f624
Regan Sarwas <rsarwas@gmail.com>
2013-12-04 21:46:47 -0900

    working on 107

--- pe6_107.hs
+++ pe6_107.hs
@@ -11,14 +11,18 @@ makeRows :: String -> [[Int]]
 makeRows s = [read (fixLine line)::[Int] | line <- lines s]
   where fixLine s =  '[' :  (map (\x -> if x == '-' then '0' else x) s) ++ "]"
 
+minimalWeight grid = sum [minimum $ filter (>0) col | col <- (drop 1 (aboveDiag grid))]
+aboveDiag grid = zipWith (\row n -> take n row) grid [0..]
+
 
 main = do
   text <- getContents
   let rows = makeRows text
       cols = transpose $ rows
       w    = (sum $ map sum rows) `div` 2
-      wmin = sum [minimum $ filter (>0) col | col <- cols]
+      wmin = minimalWeight cols
   putStrLn ("Original Weight is " ++ show w ++ ".")
+  putStrLn ("Above Diag  is " ++ (show $ aboveDiag cols) ++ ".")
   putStrLn ("Minimal Weight  is " ++ show wmin ++ ".")
   putStrLn ("Savings         is " ++ show (w - wmin) ++ ".")
 
